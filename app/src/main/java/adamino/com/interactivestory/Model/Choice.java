a3b5ec0b8cf534ff0a49fa218512c1009f25272d
Adam Hansen <adamlars90@gmail.com>
2015-07-05 19:48:04 +0200

    Initial commit

---
+++ app/src/main/java/adamino/com/interactivestory/Model/Choice.java
@@ -0,0 +1,30 @@
+package adamino.com.interactivestory.Model;
+
+/**
+ * Created by Adam on 05-07-2015.
+ */
+public class Choice {
+    private String mText;
+    private int mNextPage;
+
+    public Choice(String text, int nextPage) {
+        mText = text;
+        mNextPage = nextPage;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+}
